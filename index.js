a0ba6bd864ccee581069d55a18c717a909140bef
Cao Liang <colin3dmax@163.com>
2020-01-18 17:05:48 +0800

    更新react-native-wechat

--- index.js
+++ index.js
@@ -100,35 +100,6 @@ export const registerApp = wrapRegisterApp(WeChat.registerApp);
  * @return {Promise}
  */
 
-
-function wrapRegisterApp(nativeFunc) {
-    if (!nativeFunc) {
-        return undefined;
-    }
-    return (...args) => {
-        if (isAppRegistered) {
-            return Promise.reject(new Error('App is already registered.'));
-        }
-        isAppRegistered = true;
-        return new Promise((resolve, reject) => {
-            nativeFunc.apply(null, [
-                ...args,
-                (error, result) => {
-                    if (!error) {
-                        return resolve(result);
-                    }
-                    if (typeof error === 'string') {
-                        return reject(new Error(error));
-                    }
-                    reject(error);
-                },
-            ]);
-        });
-    };
-}
-
-
-
 export const jumpToBizProfile = WeChat.jumpToBizProfile;
 
 /**
